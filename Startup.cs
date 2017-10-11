64a44288150dbc426c1e566ad6d501e17ac66a72
Williams <twilliam@acct02.us.lmco.com>
2017-10-11 12:43:29 -0600

    Dotnet 2.0

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
